4a7899d83d33eca7517ffab683af0a227e6fea47
Petr Lobanov <petey1489@gmail.com>
2021-08-17 15:44:56 +0300

    finish increse func

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -8,11 +8,12 @@ class avl_tree
 	{
 		node * left = nullptr;
 		node * right = nullptr;
+		node * par = nullptr;
 		int hight = 1;
 		T val;
 	};
 
-	node root;
+	node * root;
 	int size;
 
 	avl_tree()
@@ -21,28 +22,83 @@ class avl_tree
 		size = 0;
 	}
 
-	bool find(T x)
+	node * find(T x)
 	{
 		node * ptr = root;
-		while (ptr != nullptr)
+		while (ptr)
 		{
 			if (ptr->val == x)
 			{
-				return true;
+				return ptr;
 			}
 			else if (x < ptr->val)
 			{
+				if (ptr->left == nullptr)
+				{
+					return ptr;
+				}
 				ptr = ptr->left;
 			}
 			else
 			{
+				if (ptr->right == nullptr)
+				{
+					return ptr;
+				}
 				ptr = ptr->right;
 			}
 		}
-		return false;
+		return ptr;
 	}
 
-	
+	void increas_hights(node * ptr)
+	{
+		while(ptr != nullptr)
+		{
+			ptr->hight++;
+			ptr = ptr->par;
+		}
+	}
+
+	void insert(T x)
+	{
+		node * ptr = find(x);
+		if (ptr == nullptr)
+		{
+			root = new node();
+			root->val = x;
+			root->hight = 1;
+			return;
+		}
+
+		if (x == ptr->val)
+		{
+			return;
+		}
+
+		if (x < ptr->val)
+		{
+			ptr->left = new node();
+			ptr->left->val = x;
+			ptr->left->par = ptr;
+		}
+		else
+		{
+			ptr->right = new node();
+			ptr->right->val = x;
+			ptr->right->par = ptr;
+		}
+		increas_hights(ptr);
+		balance(ptr);
+	}
+
+	void remove(T x)
+	{
+		node * ptr = root;
+
+	}
+
+
 
 
 
